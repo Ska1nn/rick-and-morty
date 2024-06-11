68882a80ce702e84af9294ad4ae00cba73f7969a
Ska1nn <140440954+Ska1nn@users.noreply.github.com>
2024-06-11 14:29:01 +0500

    Update project with final changes and documentation

--- src/components/CharacterCard.vue
+++ src/components/CharacterCard.vue
@@ -1,31 +1,127 @@
 <template>
-    <div class="character-card">
-      <img :src="character.image" alt="character image" />
-      <h2>{{ character.name }}</h2>
-      <p>Status: {{ character.status }}</p>
-      <p>Species: {{ character.species }}</p>
+  <div class="character-card">
+    <img :src="character.image" alt="character image" />
+    <div class="info-per">
+      <div class="name-stat">
+        <h2 style="font-size: 27px;">{{ character.name }}</h2>
+        <div class="info-status">
+          <div id="status-click" :class="statusClass"></div>
+          <p style="font-size:16px">{{ character.status }} - {{ character.species }}</p>
+        </div>
+      </div>
+      <div class="info-location">
+        <p style="font-size:16px" class="style-location">Last known location:</p>
+        <p style="font-size:18px">{{ character.location.name }}</p>
+      </div>
+      <div class="info-location">
+        <p style="font-size:16px" class="style-location">First seen in:</p>
+        <p style="font-size:18px">{{ firstEpisodeName }}</p>
+      </div>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    name: "CharacterCard",
-    props: {
-      character: Object
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CharacterCard',
+  props: {
+    character: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      firstEpisodeName: '',
+    };
+  },
+  computed: {
+    statusClass() {
+      return {
+        'status-alive': this.character.status === 'Alive',
+        'status-dead': this.character.status === 'Dead',
+        'status-unknown': this.character.status === 'unknown',
+      };
+    },
+  },
+  async mounted() {
+    if (this.character.episode.length > 0) {
+      const firstEpisodeUrl = this.character.episode[0];
+      try {
+        const response = await fetch(firstEpisodeUrl);
+        const data = await response.json();
+        this.firstEpisodeName = data.name;
+      } catch (error) {
+        console.error('Failed to fetch episode data:', error);
+      }
     }
-  };
-  </script>
-  
-  <style scoped>
-  .character-card {
-    border: 1px solid #ccc;
-    padding: 16px;
-    margin: 16px;
-    text-align: center;
-  }
-  img {
-    max-width: 100%;
-    height: auto;
-  }
-  </style>
-  
+  },
+};
+</script>
+
+<style scoped>
+.info-location {
+  font-family: -apple-system, "system-ui", "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+}
+p.style-location {
+  color: #b3b3b3;
+}
+.info-location {
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  align-items: flex-start;
+}
+.info-status {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+}
+.status-alive {
+  background: #55CB44;
+  width: 10px;
+  height: 10px;
+  border-radius: 50%;
+  margin-right: 5px;
+}
+.status-dead {
+  background: #D53C2E;
+  width: 10px;
+  height: 10px;
+  border-radius: 50%;
+  margin-right: 5px;
+}
+.status-unknown {
+  background: gray;
+  width: 10px;
+  height: 10px;
+  border-radius: 50%;
+  margin-right: 5px;
+}
+.character-card {
+  text-align: center;
+  width: 600px;
+  height: 220px;
+  display: flex;
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 6px -1px, rgba(0, 0, 0, 0.06) 0px 2px 4px -1px;
+  overflow: hidden;
+  background: rgb(60, 62, 68);
+  border-radius: 0.5rem;
+  margin: 0.75rem;
+}
+.info-per[data-v-51ed5d59] {
+    position: relative;
+    color: rgb(255, 255, 255);
+    display: flex;
+    flex-direction: column;
+    flex: 3 1 0%;
+    padding: 0.75rem;
+    align-content: center;
+    align-items: flex-start;
+    justify-content: space-around;
+}
+img {
+  max-width: 100%;
+  height: auto;
+}
+</style>
--- src/components/CharacterList.vue
+++ src/components/CharacterList.vue
@@ -1,30 +1,157 @@
 <template>
     <div>
       <div>
-        <input v-model="filters.name" placeholder="Name" />
+        <input v-model="filters.name" placeholder="Имя" />
         <select v-model="filters.status">
-          <option value="">Any Status</option>
-          <option value="alive">Alive</option>
-          <option value="dead">Dead</option>
-          <option value="unknown">Unknown</option>
+          <option value="">Любой Статус</option>
+          <option value="alive">Живой</option>
+          <option value="dead">Мертвые</option>
+          <option value="unknown">Неизвестно</option>
         </select>
-        <button @click="applyFilters">Apply</button>
+        <button class="print-search" @click="applyFilters">Применить</button>
       </div>
   
-      <div v-if="characters.length">
+      <div class="vs-info" v-if="characters.length">
         <CharacterCard v-for="character in characters" :key="character.id" :character="character" />
       </div>
       <div v-else>
-        No characters found.
+        Персонажи не найдены.
       </div>
   
-      <div>
-        <button @click="changePage(-1)" :disabled="page === 1">Previous</button>
-        <button @click="changePage(1)">Next</button>
+      <div class="arrow-6">
+        <button class="pagi-prev" @click="changePage(-1)" :disabled="page === 1">
+          <svg width="18px" height="17px" viewBox="0 0 18 17" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+        <g transform="translate(8.500000, 8.500000) scale(-1, 1) translate(-8.500000, -8.500000)">
+            <polygon class="arrow-6-pl" points="16.3746667 8.33860465 7.76133333 15.3067621 6.904 14.3175671 14.2906667 8.34246869 6.908 2.42790698 7.76 1.43613596"></polygon>
+            <polygon class="arrow-6-pl-fixed" points="16.3746667 8.33860465 7.76133333 15.3067621 6.904 14.3175671 14.2906667 8.34246869 6.908 2.42790698 7.76 1.43613596"></polygon>
+            <path d="M-1.48029737e-15,0.56157424 L-1.48029737e-15,16.1929159 L9.708,8.33860465 L-2.66453526e-15,0.56157424 L-1.48029737e-15,0.56157424 Z M1.33333333,3.30246869 L7.62533333,8.34246869 L1.33333333,13.4327013 L1.33333333,3.30246869 L1.33333333,3.30246869 Z"></path>
+        </g>
+    </svg></button>
+        <button class="pagi-next" @click="changePage(1)">
+          <svg width="18px" height="17px" viewBox="-1 0 18 17" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+        <g>
+            <polygon class="arrow-6-pl" points="16.3746667 8.33860465 7.76133333 15.3067621 6.904 14.3175671 14.2906667 8.34246869 6.908 2.42790698 7.76 1.43613596"></polygon>
+            <polygon class="arrow-6-pl-fixed" points="16.3746667 8.33860465 7.76133333 15.3067621 6.904 14.3175671 14.2906667 8.34246869 6.908 2.42790698 7.76 1.43613596"></polygon>
+            <path d="M-4.58892184e-16,0.56157424 L-4.58892184e-16,16.1929159 L9.708,8.33860465 L-1.64313008e-15,0.56157424 L-4.58892184e-16,0.56157424 Z M1.33333333,3.30246869 L7.62533333,8.34246869 L1.33333333,13.4327013 L1.33333333,3.30246869 L1.33333333,3.30246869 Z"></path>
+        </g>
+    </svg>
+  </button>
       </div>
     </div>
   </template>
-  
+  <style>
+  input {
+    background: #ffffff;
+    padding: 6px 10px;
+    border: 1px solid #272b33;
+}
+select {
+    background: #ffffff;
+    padding: 5px 8px;
+    border: 10px solid #272b33;
+}
+button.print-search {
+    background: #ff9800;
+    color: #ffffff;
+    border: 1px solid #272b33;
+    padding: 6px 10px;
+    transition: background-color 0.3s;
+}
+button.print-search:hover {
+    background: #ffffff;
+    color: #000000;
+}
+  .vs-info {
+    /* background: aqua; */
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    align-content: stretch;
+}
+  .arrow-6 {
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    align-content: stretch;
+}
+    .click-pagination {
+    background: antiquewhite;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    align-content: stretch;
+}
+button.pagi-prev {
+    background: #ff9800;
+    border: none;
+    border-radius: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+button.pagi-next {
+    background: #ff9800;
+    border: none;
+    border-radius: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+    .arrow-6 {
+    margin:20px;
+}
+.arrow-6 svg {
+    width: 20px;
+    height: auto;
+    margin: 0 2rem;
+    cursor: pointer;
+    overflow: visible;
+}
+.arrow-6 svg polygon, 
+.arrow-6 svg path {
+    transition: all 0.5s cubic-bezier(0.2, 1, 0.3, 1);
+    fill: #ffffff;
+}
+.arrow-6 svg:hover polygon, 
+.arrow-6 svg:hover path {
+    transition: all 1s cubic-bezier(0.2, 1, 0.3, 1);
+    fill: #000;
+}
+.arrow-6 svg:hover .arrow-6-pl {
+    animation: arrow-6-anim 1s cubic-bezier(0.2, 1, 0.3, 1) infinite;
+}
+.arrow-6 svg:hover .arrow-6-pl-fixed {
+    animation: arrow-6-fixed-anim 1s cubic-bezier(0.2, 1, 0.3, 1) infinite;
+}
+ 
+@keyframes arrow-6-anim {
+    0% {
+        opacity: 1;
+        transform: translateX(0);
+    }
+    5% {
+        transform: translateX(-0.1rem);
+    }
+    100% {
+        transform: translateX(1rem);
+        opacity: 0;
+    }
+}
+@keyframes arrow-6-fixed-anim {
+    5% {
+        opacity: 0;
+    }
+    20% {
+        opacity: 0.4;
+    }
+    100% {
+        opacity: 1;
+    }
+}
+  </style>
   <script>
   import axios from 'axios';
   import CharacterCard from './CharacterCard.vue';
